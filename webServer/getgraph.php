00c9ae8745416a1df3911dc4747e475f509038b2
StephanXu <xuzihanapple@live.com>
2018-12-12 23:45:25 +0800

    增加图表对比和绘制功能

---
+++ webServer/getgraph.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$username = "datacollector";
+$password = "datacollector";
+$dbname = "datacollector";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+
+$ids = $_POST['ids'];
+
+$result = $conn->query('SELECT id, result_file, result_file_content FROM datalist WHERE id IN ' . $ids) or die($conn->errno . ':' . $conn->error);
+
+$rows = $result->fetch_all();
+echo json_encode($rows);
+$conn->close();
+?>
